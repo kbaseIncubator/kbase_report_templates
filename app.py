1c7c7f995ab017afb7afee647c465cd5c23d804a
ialarmedalien <ialarmedalien@gmail.com>
2020-05-28 16:13:37 -0700

    Updating templates to use new js_wrapper Fixing all edge_data examples

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from template import Template
 from template.util import TemplateException
 import os
 from html import escape
+import json
 
 def render_template(template_name, template_data={}):
     template = Template({
@@ -15,7 +16,6 @@ def render_template(template_name, template_data={}):
         return page
     except TemplateException as e:
         print("ERROR: %s" % e)
-
 
 @route('/test')
 def test_route():
@@ -77,34 +77,72 @@ def view_template(template_name):
 
     return render_template('starter_templates/template_content', tmpl_data)
 
+@route('/example/edge_data_object')
+def edge_data_object():
+
+    source_file = os.path.join('static', 'data', 'edge_data.json')
+    with open(source_file, 'r') as read_fh:
+        lines = read_fh.read().rstrip()
+    
+    json_data = json.loads(lines)
+    tmpl_data = {
+        'data_array_of_objects': json_data,
+    }
+
+    return render_template('example/edge_data_object', tmpl_data)
+
+@route('/example/edge_data_array')
+def edge_data_array():
+
+    source_file = os.path.join('static', 'data', 'edge_data.tsv')
+
+    # read in a file, splitting on '\n' and then '\t' to create a list of lists
+    with open(source_file, 'r') as read_fh:
+        lines = list(map(lambda x: x.split('\t'), read_fh.read().rstrip().split('\n')))
+
+    headers = lines[0]
+    output_data = list(map( lambda x: dict(zip(headers, x)), lines[1:]))
+    with open(os.path.join('static', 'data', 'edge_data_json.json'), 'w') as write_fh:
+        write_fh.write(json.dumps( output_data ) )
+
+    tmpl_data = {
+        # lines[0] is the header line
+        # the rest of the lines are the data points
+        'data_array_of_arrays': lines[1:],
+    }
+
+    return render_template('example/edge_data_array', tmpl_data)
+
+@route('/example/edge_data_tsv_file')
+def edge_data_tsv_file():
+
+    return render_template(
+        'example/edge_data_tsv_file',
+        { 'file_path': '/static/data/edge_data.tsv' }
+    )
+
+@route('/example/edge_data_json_file')
+def edge_data_json_file():
+
+    return render_template(
+        'example/edge_data_json_file', 
+        { 'file_path': '/static/data/edge_data.json' }
+    )
 
 @route('/example/kb_uploadmethods_import_genbank')
 def kb_uploadmethods_import_genbank():
 
     tmpl_data = {
         'upload_data': [
-            [ "Name", "Escherichia_coli_str_K-12_MG1655 (55494/26/3)" ],
-            [ "Date Uploaded", "Sat Feb 22 19:08:55 2020" ],
-            [ "Source", "RefSeq" ],
-            [ "Number of Contigs", "1" ],
-            [ "Size", "4641652" ],
-            [ "GC Content", "0.50791" ],
-            [ "Warnings", "" ],
-            [ "CDS", "4319" ],
-            [ "gene", "4498" ],
-            [ "misc_feature", "11" ],
-            [ "mobile_element", "49" ],
-            [ "ncRNA", "65" ],
-            [ "non_coding_features", "773" ],
-            [ "non_coding_genes", "179" ],
-            [ "protein_encoding_gene", "4319" ],
-            [ "rRNA", "22" ],
-            [ "rep_origin", "1" ],
-            [ "repeat_region", "355" ],
-            [ "tRNA", "89" ],
-            [ "tmRNA", "2" ],
-        ],
-        'genome_features': [
+            ['Name', 'Escherichia_coli_str_K-12_MG1655 (55494/26/3)'],
+            ['Date Uploaded', 'Sat Feb 22 19:08:55 2020'],
+            ['Source', 'RefSeq'],
+            ['ID', 'NC_000913.3'],
+            ['Number of Contigs', 1],
+            ['Size', 4641652 ],
+            ['GC Content', '0.50791'],
+            ['Warnings', ''],
+            ['gene', 4498],
             ['CDS', 4319],
             ['misc_feature', 11],
             ['mobile_element', 49],
@@ -120,7 +158,6 @@ def kb_uploadmethods_import_genbank():
         ],
         'contig_length': [
             ['NC_000913.3', 4641652],
-            ['NC_002381.7', 82981],
         ]
     }
 
@@ -182,28 +219,6 @@ def kb_trimmomatic_single():
     }
 
     return render_template('example/kb_trimmomatic', tmpl_data)
-
-@route('/example/edge_data_array')
-def edge_data_array():
-
-    source_file = os.path.join('data', 'edge_data.tsv')
-
-    # read in a file, splitting on '\n' and then '\t' to create a list of lists
-    with open(source_file, 'r') as read_fh:
-        lines = list(map(lambda x: x.split('\t'), read_fh.read().rstrip().split('\n')))
-
-    tmpl_data = {
-        # lines[0] is the header
-        # the rest of the lines are the data points
-        'parsed_data_from_file': lines[1:],
-    }
-
-    return render_template('starter_templates/edge_data_array', tmpl_data)
-
-@route('/example/edge_data_tsv_file')
-def edge_data_tsv_file():
-
-    return render_template('starter_templates/edge_data_tsv_file', { 'file_path': '/static/data/edge_data.tsv' })
 
 @route('/example/<template_name>')
 def default_route(template_name):
